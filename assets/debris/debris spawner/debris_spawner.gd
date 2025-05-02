16a9908d53ea0e079fc62bf5b7ea6c43b5705a5d
Jupi <spacejupiter0@gmail.com>
2025-05-02 21:46:56 +0300

    debris spawn distances fixes

--- assets/debris/debris spawner/debris_spawner.gd
+++ assets/debris/debris spawner/debris_spawner.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
 @onready var ship_camera: Camera2D = get_node("%Ship/RigidBody2D/Camera2D")
+@onready var walls: Array[Node] = get_node("%Walls/RigidBody2D/Left Right").get_children()
 @export var debris_objects: Array[PackedScene] = []
 
 func _spawn_batch() -> void:
 	var viewport_size = get_viewport().get_visible_rect().size
-	var generated_points = PoissonDiscSampling.generate_points(160, Vector2(viewport_size.x, 1000), 30)
+	var width = abs(walls[0].global_position.x - walls[1].global_position.x)
+	var generated_points = PoissonDiscSampling.generate_points(160, Vector2(width, 1000), 30)
 
 	for generated_point in generated_points:
-		_spawn_debris(generated_point + Vector2(-viewport_size.x, 0))
+		_spawn_debris(generated_point + Vector2(-820, 0))
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
