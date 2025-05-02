d7316209479e0a99c70eb3036d06b04e45afcce8
Jupi <spacejupiter0@gmail.com>
2025-05-02 18:37:53 +0300

    debris spawning with W

---
+++ assets/debris/debris spawner/debris_spawner.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@onready var ship_camera: Camera2D = get_node("%Ship/RigidBody2D/Camera2D")
+@export var debris_objects: Array[PackedScene] = []
+
+func _spawn_batch() -> void:
+	var viewport_size = get_viewport().get_visible_rect().size
+	var generated_points = PoissonDiscSampling.generate_points(160, Vector2(viewport_size.x, 1000), 30)
+
+	for generated_point in generated_points:
+		_spawn_debris(generated_point + Vector2(-viewport_size.x, 0))
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_W:
+			#var viewport_size = get_viewport_rect().size
+			#var top_center_screen_pos = Vector2(viewport_size.x / 2, 0)  # Top center of the screen
+			#var top_center_world_pos = ship_camera.get_global_transform().affine_inverse() * top_center_screen_pos
+
+			_spawn_batch()
+			queue_redraw()
+
+func _spawn_debris(spawn_position: Vector2):
+	var debris_scene = debris_objects.pick_random()
+	var debris_scene_instance = debris_scene.instantiate()
+	debris_scene_instance.global_position = spawn_position
+	add_child(debris_scene_instance)
--- assets/managers/game manager/game_manager.gd
+++ assets/managers/game manager/game_manager.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
-@export var score: int = 0;
-@export var score_increment_amount: int = 1;
+class_name GameManager
 
+signal score_changed(old_value: int, new_value: int)
 
-func _physics_process(delta: float) -> void:
-	score += 1;
-
+@export var score: int = 0
+@export var score_increment_amount: int = 1
+@export var base_travelling_speed: float = 100.0
 
 func _on_score_timer_timeout() -> void:
-	var currentScore: int = score;
-	score = currentScore + score_increment_amount;
+	increaseScore()
 
-signal score_change(old_value: int, new_value: int)
+func increaseScore(amount: int = score_increment_amount):
+	var currentScore: int = score
+	score = currentScore + amount
+	emit_signal("score_changed", currentScore, score)
