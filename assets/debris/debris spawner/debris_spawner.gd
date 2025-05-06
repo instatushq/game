eba18434307ed045cf02040d8ffaca8a780ef8f9
Jupi <spacejupiter0@gmail.com>
2025-05-06 18:53:33 +0300

    debris spawn balancing

--- assets/debris/debris spawner/debris_spawner.gd
+++ assets/debris/debris spawner/debris_spawner.gd
@@ -7,19 +7,19 @@ var spawnPositions: Array[Vector2] = []
 
 var grid_size_draw: Vector2 = Vector2(0, 0)
 var grid_position_draw: Vector2 = Vector2(0, 0)
-const distance_from_camera_top_to_spawn_batch: float = 600
-const DISTANCE_BETWEEN_SEGMENTS: float = 1000
+const distance_from_camera_top_to_spawn_batch: float = 0
+const DISTANCE_BETWEEN_SEGMENTS: float = 100
 var next_spawn_y: float = 0
 var last_spawn_y: float = 0
 var can_spawn: bool = true
 
 func _spawn_batch(y_level: float = 0) -> void:
-	var view_port_y_size = get_viewport_rect().size.y
-	var area_width = 530
+	var view_port_y_size = 600
+	var area_width = 400
 	var grid_size = Vector2(area_width, view_port_y_size)
 	
-	var center_x_position = (walls[0].position.x + walls[1].position.x) / 2
-	var grid_position = Vector2(center_x_position, y_level - grid_size.y)
+	var center_x_position = 150
+	var grid_position = Vector2(center_x_position, y_level - grid_size.y - 150)
 	grid_size_draw = grid_size
 	grid_position_draw = grid_position
 	last_spawn_y = y_level
@@ -27,7 +27,7 @@ func _spawn_batch(y_level: float = 0) -> void:
 	# Set the next spawn trigger position based on camera distance
 	next_spawn_y = y_level - grid_size.y - distance_from_camera_top_to_spawn_batch
 	
-	var generated_points = PoissonDiscSampling.generate_points(150, grid_size, 50, grid_position)
+	var generated_points = PoissonDiscSampling.generate_points(170, grid_size, 50, grid_position)
 
 	for point in generated_points:
 		spawnPositions.append(point)
@@ -68,9 +68,10 @@ func _ready() -> void:
 	var viewport_size = get_viewport_rect().size
 	var top_center_screen_pos = Vector2(viewport_size.x / 2, 0)
 	var top_center_world_pos = ship_camera.get_global_transform().affine_inverse() * top_center_screen_pos
-	_spawn_batch(-top_center_world_pos.y)
+	_spawn_batch(-top_center_world_pos.y * -10)
 	
 func _draw():
-	draw_rect(Rect2(Vector2(grid_position_draw.x - grid_size_draw.x, grid_position_draw.y - grid_size_draw.y), Vector2(grid_size_draw.x * 2, grid_size_draw.y * 2)), Color(0, 0, 0, 0.5), true)
-	for spawn in spawnPositions:
-		draw_circle(spawn, 16, Color.LIGHT_PINK, true)
+	pass
+	# draw_rect(Rect2(Vector2(grid_position_draw.x - grid_size_draw.x, grid_position_draw.y - grid_size_draw.y), Vector2(grid_size_draw.x * 2, grid_size_draw.y * 2)), Color(0, 0, 0, 0.5), true)
+	# for spawn in spawnPositions:
+		# draw_circle(spawn, 16, Color.LIGHT_PINK, true)
--- assets/debris/debris.gd
+++ assets/debris/debris.gd
@@ -1,5 +1,33 @@
 extends Node2D
 
+var ship_direction: Vector2 = Vector2.ZERO
+@onready var rb: ShipImpacter = $"Ship Impacter"
+var game_manager: GameManager = null
+
+var SPEED: int = 9
+
+func _ready() -> void:
+	var ship: Ship = get_tree().get_current_scene().get_node("%Ship")
+	var ship_rb: RigidBody2D = ship.get_node("RigidBody2D")
+	var angle = rb.global_position.angle_to_point(ship_rb.global_position)
+	var new_velocity = Vector2.from_angle(angle) * SPEED
+	rb.linear_velocity = new_velocity
+	ship_direction = new_velocity
+	game_manager = get_tree().get_current_scene().get_node("%GameManager")
+	var distance_to_ship = rb.global_position.distance_to(ship_rb.global_position)
+	print(distance_to_ship)
+	if distance_to_ship > 500:
+		SPEED = 13
+	
+func _physics_process(_delta: float) -> void:
+	if game_manager.current_player == GameManager.Player.SHIP:
+		rb.linear_velocity = ship_direction * SPEED
+	else:
+		rb.linear_velocity = Vector2.ZERO
+
 func _on_ship_impacter_on_shot(laser_bullet: Node2D) -> void:
 	queue_free()
 	laser_bullet.queue_free()
+
+func _on_ship_impacter_on_impact_ship(_ship: Node2D, _current_impact_count: int, _max_impact_count: int) -> void:
+	queue_free()
