88eabbd329e9856f40a00150df9688b82434dcfd
Jupi <spacejupiter0@gmail.com>
2025-05-06 11:48:22 +0300

    added on shot events

--- assets/components/ship_impacter/ship_impacter.gd
+++ assets/components/ship_impacter/ship_impacter.gd
@@ -9,6 +9,7 @@ class_name ShipImpacter
 var amount_of_impacts = 0;
 
 signal on_impact_ship(ship: Node2D, current_impact_count: int, max_impact_count: int)
+signal on_shot(body: Node2D)
 
 func _ready():
 	body_entered.connect(_on_body_entered)
@@ -17,3 +18,5 @@ func _on_body_entered(body) -> void:
 	if body.get_parent().name.to_lower() == "ship" and amount_of_impacts < maximum_amount_of_impacts:
 		amount_of_impacts += 1
 		on_impact_ship.emit(body.get_parent(), amount_of_impacts, maximum_amount_of_impacts)
+	elif body is Pew:
+		on_shot.emit(body)
