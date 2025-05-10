8fa1e456c4b28a45aa32002134b63cf09b0572bb
Jupi <spacejupiter0@gmail.com>
2025-05-10 17:24:59 +0300

    prevented shooting off camera

--- assets/components/ship_impacter/ship_impacter.gd
+++ assets/components/ship_impacter/ship_impacter.gd
@@ -14,7 +14,15 @@ signal on_shot(body: Node2D)
 func _ready():
 	body_entered.connect(_on_body_entered)
 
-func _on_body_entered(body) -> void:	
+func _on_body_entered(body) -> void:
+	var viewport = get_viewport()
+	var viewport_rect = viewport.get_visible_rect()
+	var viewport_pos = viewport.get_canvas_transform() * global_position
+	if not viewport_rect.has_point(viewport_pos):
+		if body is Pew:
+			body.queue_free()
+		return
+		
 	if body.get_parent().name.to_lower() == "ship" and amount_of_impacts < maximum_amount_of_impacts:
 		amount_of_impacts += 1
 		on_impact_ship.emit(body.get_parent(), amount_of_impacts, maximum_amount_of_impacts)
