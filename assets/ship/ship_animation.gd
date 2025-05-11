0626d3a2fb1845fce40386673973727326fcb7c1
Jupi <spacejupiter0@gmail.com>
2025-05-11 17:58:42 +0300

    added animations for ship

--- assets/ship/ship_animation.gd
+++ assets/ship/ship_animation.gd
@@ -2,3 +2,23 @@ extends AnimatedSprite2D
 
 @onready var trail_sprite: AnimatedSprite2D = $Trail
 @onready var light_sprite: AnimatedSprite2D = $Light
+@onready var fuel: ShipFuel = $"../../Fuel"
+
+var is_fueled_up: bool = true
+
+func _ready() -> void:
+	fuel.on_fuel_change.connect(_on_fuel_change)
+
+func _on_fuel_change(_old_fuel: float, new_fuel: float) -> void:
+	if new_fuel <= 0 and is_fueled_up:
+		is_fueled_up = false
+		_on_fueled_status_change(is_fueled_up)
+	elif new_fuel > 0 and not is_fueled_up:
+		is_fueled_up = true
+		_on_fueled_status_change(is_fueled_up)
+
+func _on_fueled_status_change(is_fueled: bool) -> void:
+	if !is_fueled:
+		light_sprite.play("fuel_end")
+		trail_sprite.play("fuel_end")
+		play("fuel_end")
