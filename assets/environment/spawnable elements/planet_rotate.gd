2480c21688aafe32ca2160ba2d54c3fcb85e9084
Jupi <spacejupiter0@gmail.com>
2025-05-13 00:05:03 +0300

    adjusted parallax

--- assets/environment/spawnable elements/planet_rotate.gd
+++ assets/environment/spawnable elements/planet_rotate.gd
@@ -1,11 +1,14 @@
 extends ParallaxLayer
 
-@onready var visual_container = $Container
+@onready var visual_container: Node2D = $Container
+@onready var ring: Node2D = $Container/Ring
 var rotation_direction_movement: bool = false
 @export var rotation_speed: float = 0.0003
 
 func _ready() -> void:
 	rotation_direction_movement = randi_range(0, 100) > 50
+	visual_container.scale = Vector2(1 if randi_range(0, 100) > 50 else -1, 1)
+	ring.visible = randi_range(0, 100) > 50
 
 func _physics_process(_delta: float) -> void:
 	if rotation_direction_movement:
