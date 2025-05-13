ad79b604c784be830e70dc87c9f360849e802a21
Jupi <spacejupiter0@gmail.com>
2025-05-13 14:55:48 +0300

    planet rotate improvement

--- assets/environment/spawnable elements/planet_rotate.gd
+++ assets/environment/spawnable elements/planet_rotate.gd
@@ -5,15 +5,24 @@ extends ParallaxLayer
 @export var rotation_speed: float = 0.0003
 var rotation_direction_movement: bool = false
 
-func _ready() -> void:
-	randomize_planets_look()
-
 func randomize_planets_look() -> void:
 	rotation_direction_movement = randi_range(0, 100) > 50
 	visual_container.scale = Vector2(1 if randi_range(0, 100) > 50 else -1, 1)
 	ring.visible = randi_range(0, 100) > 50
 
 func _physics_process(_delta: float) -> void:
+	var camera = get_viewport().get_camera_2d()
+	var camera_y = int(round(camera.global_position.y))
+	var motion_mirroring_y = int(round(motion_mirroring.y))
+	var absolute_normalized_distance = abs(camera_y % motion_mirroring_y)
+	if name.contains("Big"):
+		print(camera_y % motion_mirroring_y)
+		
+	if absolute_normalized_distance <= 2:
+		randomize_planets_look()
+		if name.contains("Big"):
+			print("helloo")
+		
 	if rotation_direction_movement:
 		visual_container.rotation += rotation_speed
 	else:
