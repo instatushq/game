deff928d5fb2e6b332b02a9e56efbe6366b70c3a
Jupi <spacejupiter0@gmail.com>
2025-05-13 12:03:02 +0300

    added function to randomize planets looks

--- assets/environment/spawnable elements/planet_rotate.gd
+++ assets/environment/spawnable elements/planet_rotate.gd
@@ -2,10 +2,13 @@ extends ParallaxLayer
 
 @onready var visual_container: Node2D = $Container
 @onready var ring: Node2D = $Container/Ring
-var rotation_direction_movement: bool = false
 @export var rotation_speed: float = 0.0003
+var rotation_direction_movement: bool = false
 
 func _ready() -> void:
+	randomize_planets_look()
+
+func randomize_planets_look() -> void:
 	rotation_direction_movement = randi_range(0, 100) > 50
 	visual_container.scale = Vector2(1 if randi_range(0, 100) > 50 else -1, 1)
 	ring.visible = randi_range(0, 100) > 50
