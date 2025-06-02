d30b4e03ca4ce65f4d501239a289dd46342b93e0
Jupi <spacejupiter0@gmail.com>
2025-06-03 00:06:19 +0300

    added environment variables to URL

--- assets/game over/game_over.gd
+++ assets/game over/game_over.gd
@@ -2,12 +2,16 @@ extends Node2D
 
 @onready var http: HTTPRequest = $HTTPRequest
 @onready var leaderboard: LeaderboardUI = $Camera2D/Leaderboard
+var base_url: String = "http://localhost:3000"
 
 func _ready() -> void:
+	if OS.has_environment("LEADERBOARD_URL"): base_url = OS.get_environment("LEADERBOARD_URL")
 	http.request_completed.connect(_on_request_complete)
-	http.request("http://localhost:3000/leaderboard")
+	http.set_tls_options(TLSOptions.client_unsafe())
+	http.request(base_url+"/leaderboard")
 
 func _on_request_complete(_result: int, _response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
+	print(body.get_string_from_utf8())
 	var json = JSON.parse_string(body.get_string_from_utf8())
 	if json == null:
 		push_error("Failed to parse JSON response")
@@ -20,5 +24,6 @@ func _on_request_complete(_result: int, _response_code: int, _headers: PackedStr
 	leaderboard.set_entries_data(entries)
 
 func _on_leaderboard_on_added_new_score() -> void:
-	http.request("http://localhost:3000/leaderboard")
+	http.set_tls_options(TLSOptions.client_unsafe())
+	http.request(base_url+"/leaderboard")
 	print("updating leaderboard view")
