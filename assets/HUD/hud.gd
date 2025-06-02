ae72eef72411980189157ffc2fb45ed3c612e55a
Jupi <spacejupiter0@gmail.com>
2025-06-02 18:13:58 +0300

    HUD

--- assets/HUD/hud.gd
+++ assets/HUD/hud.gd
@@ -1,19 +1,29 @@
 extends CanvasLayer
 
+@export var score_for_complete_bar: int = 500
+@onready var score_xp_bar: ColorRect = $HUDUI/XP/BarContainer/Bar
 @onready var health_label: Label = $HUDUI/Health/HealthContainer/HealthLabel
 @onready var health_bar_color_rects_container: VBoxContainer = $HUDUI/Bars
-@onready var score_label: Label = $TextureRect/ScoreContainer/Score
+@onready var health_arrows: Label = $HUDUI/Health/Arrows
+@onready var systems_label: Label = $HUDUI/Systems
+@onready var systems_label_font_size: int = systems_label.label_settings.font_size
+@onready var score_label: Label = $HUDUI/XP/Score
 @onready var game_manager: GameManager = %GameManager
 @onready var ship: ShipHealth = %InternalShip/Health
 @onready var issues: Issues = %InternalShip/Issues
 @onready var bars: Array[ColorRect] = []
+@onready var dialog_box: DialogBox = $HUDUI/DialogBox
+@onready var time_label: Label = $HUDUI/DialogBox/Clock
 
+enum COMPUTER_EMOTION {
+	DEFAULT
+}
 
-const HEALTH_COLORS = [
-	{ "threshold": 75, "text": "All Systems Operational" },
-	{ "threshold": 50, "text": "Degraded Systems" },
-	{ "threshold": 25, "text": "Partial Outage" },
-	{ "threshold": 0, "text": "Major Outage" }
+const SYSTEMS_TITLES = [
+	{ "threshold": 75, "text": "All Systems\nOperational", "font_size_adjustment": 0 },
+	{ "threshold": 50, "text": "All Systems\nDegraded", "font_size_adjustment": 0 },
+	{ "threshold": 25, "text": "Systems\nPartially Outaged", "font_size_adjustment": -4 },
+	{ "threshold": 0, "text": "Systems\nMajorly Outaged", "font_size_adjustment": -4 }
 ]
 
 const BARS_COLORS = [
@@ -22,7 +32,15 @@ const BARS_COLORS = [
 	{ "bars_count": 4, "color": Color("72bcf2")},
 ]
 
+func _physics_process(_delta: float) -> void:
+	var time_dict = Time.get_datetime_dict_from_system()
+	var hour = time_dict.hour % 12
+	if hour == 0:
+		hour = 12
+	time_label.text = str(hour) + ":" + str(time_dict.minute).pad_zeros(2) + " " + ("AM" if time_dict.hour < 12 else "PM")
+
 func _ready():
+	_update_systems_titles(100.0)
 	if game_manager != null:
 		game_manager.score_changed.connect(on_score_change)
 	if ship != null:
@@ -40,7 +58,11 @@ func _ready():
 			current_colors_index += 1
 			current_color = BARS_COLORS[current_colors_index].color
 
-func _on_health_change(_old_health: float, new_health: float) -> void:
+func _on_health_change(old_health: float, new_health: float) -> void:
+	_update_systems_titles(new_health)
+	if old_health != new_health:
+		health_arrows.rotation_degrees = 180 if new_health > old_health else 90
+	
 	var color_rects = health_bar_color_rects_container.get_children()
 	health_label.text = str(int(new_health))
 	var percent_per_bar: float = 100.0 / health_bar_color_rects_container.get_child_count()
@@ -51,6 +73,20 @@ func _on_health_change(_old_health: float, new_health: float) -> void:
 		else:
 			color_rects[color_rects.size() - i - 1].visible = true
 
+func _update_systems_titles(new_health: float) -> void:
+	for title in SYSTEMS_TITLES:
+		if new_health > title.threshold:
+			systems_label.text = title.text
+			systems_label.label_settings.font_size = systems_label_font_size + title.font_size_adjustment
+			break
+
+func _portray_emotion(emotion: COMPUTER_EMOTION, dialog: String) -> void:
+	dialog_box.speak_words(dialog, "E N.Stat", 0.5, 1)
 
 func on_score_change(_old_screen: int, new_score: int):
-	pass
+	score_label.text = str(new_score)
+	var remainder = fmod(new_score, score_for_complete_bar)
+	var completion_percentage = remainder / score_for_complete_bar
+	score_xp_bar.scale.x = completion_percentage
+	if completion_percentage <= 0.01:
+		dialog_box.speak_words("Conputer Is Here. \n I Am Conputer. But I am Good Conputer.", "E N.Stat", 0.1, 1)
