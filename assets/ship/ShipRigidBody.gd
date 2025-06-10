dfb75bde5ae3ff493fdd61906e17cb0a9b252407
Jupi <spacejupiter0@gmail.com>
2025-06-10 19:56:38 +0300

    fixed ship keyboard movement

--- assets/ship/ShipRigidBody.gd
+++ assets/ship/ShipRigidBody.gd
@@ -1,14 +1,20 @@
-extends RigidBody2D
+class_name ShipRigidBody extends RigidBody2D
 
-class_name ShipRigidBody
+var game_position: Vector2 = Vector2.ZERO
 
 signal on_impact(impacter: ShipImpacter)
 
 func _ready():
 	body_entered.connect(_on_body_entered)
 
+func enforce_global_position(rb_position: Vector2) -> void:
+	game_position = rb_position
+
 func _on_body_entered(body) -> void:
 	if body is ShipImpacter:
 		var ship_impacter: ShipImpacter = body;
 		if ship_impacter.amount_of_impacts < ship_impacter.maximum_amount_of_impacts:
 			on_impact.emit(ship_impacter)
+			
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	state.transform.origin = game_position
--- assets/ship/ship.gd
+++ assets/ship/ship.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 class_name Ship
 
-@export var movement_speed: float = 100.0
+@export var movement_speed: float = 20
 @export var bottom_camera_movement_margin: float = 100.0
 var side_movement_padding: float = 48.0
 
@@ -48,59 +48,34 @@ func _process(delta: float) -> void:
 					_fire_cannons()
 				can_fire = false
 
-func _input(event: InputEvent) -> void:
-	if game_manager.is_playing == false: return
-	if event is InputEventMouseMotion:
-		is_keyboard_controlled = false
-		y_position_synced = false
-	elif event is InputEventKey:
-		is_keyboard_controlled = true
-		if y_position_synced == false:
-			mouse_world_position = get_global_mouse_position()
-			ship_position.y = mouse_world_position.y + 50
-			y_position_synced = true
-
 func _physics_process(_delta: float) -> void:
-	if game_manager.is_playing == false: return
-
-	var viewport_size = get_viewport_rect().size
+	var input_axis = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
 	var camera = get_viewport().get_camera_2d()
-	
-	var top_edge = camera.global_position.y - viewport_size.y / 2 * camera.zoom.y
-	var bottom_edge = camera.global_position.y + viewport_size.y / 2 * camera.zoom.y
-	var right_edge = camera.global_position.x + viewport_size.x / 2 * camera.zoom.x
-	var left_edge = camera.global_position.x - viewport_size.x / 2 * camera.zoom.x
-	
-	if not is_keyboard_controlled:
-		mouse_world_position = get_global_mouse_position()
-		const vertical_ship_padding = 50
-		var restricted_y = clamp(mouse_world_position.y + vertical_ship_padding, camera.global_position.y, bottom_edge - 1000)
-		var restricted_x = clamp(mouse_world_position.x, left_edge + 1400, right_edge - 1400)
-		rb.global_position = Vector2(restricted_x, restricted_y)
-		if Input.is_action_pressed("fire_cannon") and can_fire:
-			_fire_cannons()
-			can_fire = false
 
-	if is_keyboard_controlled:
-		rb.global_position = camera.global_position + ship_position
-		var verticalDirection := Input.get_axis("move_up", "move_down")
-		if verticalDirection < 0:
-			if camera.global_position.y + ship_position.y > top_edge + 1700:
-				ship_position.y -= 12
-		elif verticalDirection > 0:
-			if camera.global_position.y + ship_position.y < bottom_edge - 1000:
-				ship_position.y += 8
-				
-		var direction := Input.get_axis("move_left", "move_right")
-		if direction < 0:
-			if (ship_position.x > left_edge + 1500):
-				ship_position.x -= 10
-		elif direction > 0:
-			if (ship_position.x < right_edge - 1500):
-				ship_position.x += 10
-		if Input.is_action_pressed("fire_cannon") and can_fire:
-			_fire_cannons()
-			can_fire = false
+	_handle_keyboard_movement(input_axis, camera)
+
+	var final_ship_position = camera.global_position + ship_position
+	
+	rb.enforce_global_position(final_ship_position)
+
+	if Input.is_action_pressed("fire_cannon") and can_fire:
+		_fire_cannons()
+		can_fire = false
+
+func _handle_keyboard_movement(input_axis: Vector2, camera: Camera2D) -> void:
+	var viewport_size = get_viewport_rect().size
+	var bottom_edge = ((camera.global_position.y + (viewport_size.y / camera.zoom.y) / 2))
+	var right_edge = ((camera.global_position.x + (viewport_size.x / camera.zoom.x) / 2))
+	var left_edge = ((camera.global_position.x - viewport_size.x) / camera.zoom.x) / 2
+	var final_ship_position_unrestricted = ship_position + (input_axis * movement_speed)
+	var final_ship_global_position = camera.global_position + final_ship_position_unrestricted
+
+	var x_position_restricted = clamp(final_ship_global_position.x, left_edge + side_movement_padding, right_edge - side_movement_padding)
+	var bottom_edge_to_center = bottom_edge - camera.global_position.y
+	var y_position_restricted = clamp(final_ship_position_unrestricted.y, bottom_edge_to_center - bottom_camera_movement_margin, bottom_edge_to_center)
+	
+	if game_manager.is_playing:
+		ship_position = Vector2(x_position_restricted - camera.global_position.x, y_position_restricted)
 
 func toggle_control(new_can_control: bool) -> void:
 	can_control = new_can_control
