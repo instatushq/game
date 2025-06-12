b6b636f0156daca6ded67fa83018dff40c7fd0d9
Jupi <spacejupiter0@gmail.com>
2025-06-12 16:20:56 +0300

    fixed crashes

--- assets/managers/environment manager/environment_manager.gd
+++ assets/managers/environment manager/environment_manager.gd
@@ -3,16 +3,17 @@ class_name EnvironmentManager extends Node2D
 @onready var background_controller: ParallaxBackgroundController = %ParallaxBackground
 var list_of_celestials: Array = []
 @export var max_background_segments: int = 3
-var camera: Camera2D
+@export var camera: Camera2D
 var last_recorded_y: float = 0
 const SEGMENT_DISTANCE: float = 400.0
 
 func _ready() -> void:
-	camera = get_viewport().get_camera_2d()
 	_spawn_and_save_celestial_objects()
 
 func _spawn_and_save_celestial_objects() -> void:
-	last_recorded_y = camera.global_position.y
+	if is_instance_valid(camera):
+		last_recorded_y = camera.global_position.y
+
 	var viewport_size = get_viewport_rect().size
 	list_of_celestials.append(background_controller.generate_background_segment(camera.global_position.y, camera.global_position.x + (viewport_size.x / 4), viewport_size.y, viewport_size.x))
 	if list_of_celestials.size() == 3:
@@ -22,10 +23,9 @@ func _spawn_and_save_celestial_objects() -> void:
 
 		list_of_celestials.remove_at(0)
 	
-	queue_redraw()
-
 func _physics_process(_delta: float) -> void:
-	var viewport_size = get_viewport_rect().size
-	var camera_top_border_y = camera.global_position.y - (viewport_size.y / 4)
-	if camera_top_border_y < last_recorded_y - viewport_size.y - SEGMENT_DISTANCE:
-		_spawn_and_save_celestial_objects()
+	if is_instance_valid(camera):
+		var viewport_size = get_viewport_rect().size
+		var camera_top_border_y = camera.global_position.y - (viewport_size.y / 4)
+		if camera_top_border_y < last_recorded_y - viewport_size.y - SEGMENT_DISTANCE:
+			_spawn_and_save_celestial_objects()
