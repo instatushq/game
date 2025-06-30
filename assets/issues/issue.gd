639b7f1fa61a8cbd68810f03d63d6032145ac1a7
Jupi <spacejupiter0@gmail.com>
2025-06-30 12:49:34 +0300

    added aborting uI

--- assets/issues/issue.gd
+++ assets/issues/issue.gd
@@ -1,15 +1,20 @@
-extends Node2D
-
-class_name Issue
+class_name Issue extends Node2D
 
 @export_range(0, 100) var min_hp_revive: int = 20
 @export_range(0, 100) var max_hp_revive: int = 25
 
 @export var default_visibility: bool = false
 @export var spawn_position: Vector2 = Vector2(5000, 5000)
+@export var allow_abort: bool = true
+@export var abort_font: Font = null
+var abort_ui: PackedScene = preload("res://assets/issues/abort_ui.tscn")
+@onready var abort_ui_instance: CanvasLayer = null
+@onready var abort_loading_bar = null
 @onready var custom_camera: Camera2D = null
 @onready var canvas_layer: CanvasLayer = null
 @onready var scene_root = get_tree().root.get_child(get_tree().root.get_children().size() - 1)
+@onready var abort_timer: Timer = Timer.new()
+
 var main_camera: Camera2D = null
 var is_open: bool = false
 
@@ -41,6 +46,7 @@ func _ready() -> void:
 	issue_resolved.connect(close_issue)
 	issue_failed.connect(close_issue)
 	issue_aborted.connect(abort_issue)
+	abort_timer.timeout.connect(abort_issue)
 	custom_camera = _find_camera2d(self)
 	canvas_layer = _find_canvas_layer(self)
 	var game_manager: GameManager = scene_root.get_node("%GameManager")
@@ -53,8 +59,16 @@ func _ready() -> void:
 	global_position = spawn_position
 	visible = default_visibility
 	
+	abort_timer.one_shot = true
+	add_child(abort_timer)
+
+	abort_ui_instance = abort_ui.instantiate()
+	add_child(abort_ui_instance)
+	abort_ui_instance.visible = false
+	abort_loading_bar = abort_ui_instance.get_node("Container/Background/BarBG2")
+
 	# for testing bugs
-	open_issue()
+	# open_issue()
 
 func open_issue() -> void:
 	is_open = true
@@ -76,10 +90,22 @@ func close_issue() -> void:
 	issue_closed.emit()
 
 func _input(event: InputEvent) -> void:
+	if not allow_abort: return
 	if event.is_action_pressed("abort"):
-		abort_issue()
+		if abort_timer.is_stopped() or abort_timer.paused:
+			abort_timer.start(1.5)
+			abort_ui_instance.visible = true
+	elif event.is_action_released("abort"):
+		abort_timer.stop()
+		abort_ui_instance.visible = false
+
+func _physics_process(_delta: float) -> void:
+	if not abort_timer.paused and not abort_timer.is_stopped():
+		var time_progress = 1 - (abort_timer.time_left / abort_timer.wait_time)
+		abort_loading_bar.scale.x = time_progress
 
 func abort_issue() -> void:
+	abort_ui_instance.visible = false
 	if is_open:
 		close_issue()
 		issue_aborted.emit()
